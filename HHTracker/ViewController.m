d103eb86ba1992f784a7d964162afe89736e70de
蔡万鸿 <caiwanhong@koolearn.com>
2019-08-07 16:14:06 +0800

    Initial Commit

---
+++ HHTracker/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HHTracker
+//
+//  Created by wanhong cai on 2019/8/7.
+//  Copyright © 2019 koolearn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ HHTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HHTracker
+//
+//  Created by wanhong cai on 2019/8/7.
+//  Copyright © 2019 koolearn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
